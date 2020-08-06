f1c6c9fa0bfe1859d7e764d7a7ce0b08b7a62817
Euler1729 <44305089+Euler1729@users.noreply.github.com>
2020-08-06 15:23:08 +0530

    Create 1399A.cpp

---
+++ CF/Contests/#661 (DIV 3) 1399/1399A/1399A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+int main()
+{
+ 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //freopen("input.txt","r", stdin);
+    //freopen("output.txt","w",stdout); 
+    int t; cin >> t;
+    while (t--) { 
+        int n; cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        sort(a, a+n);
+ 
+        if (n == 1) {
+            printf("YES\n");
+            continue;
+        }
+        int b = 1;
+        for (int i = 1; i < n; i++) {
+            if (abs(a[i] - a[i-1]) > 1) {
+                b = 0;
+                break;
+            }
+        }
+        if (b) printf("YES\n");
+        else printf("NO\n");
+    }
+}   
