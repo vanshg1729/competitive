89059472e6d39ab191c1c4b44edf8593242a06f1
Euler1729 <44305089+Euler1729@users.noreply.github.com>
2020-08-15 01:08:51 +0530

    Create 1398A.cpp

---
+++ CF/Contests/EDU 92 (Div 2) 1398/1398A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+int main()
+{
+   cin.tie(0);
+   cout.tie(0);
+   //freopen("input.txt","r",stdin);
+   //freopen("output.txt","w",stdout);
+   int t; cin>>t;
+   while(t--)
+   {
+   	 int n; cin>>n;
+   	 int a[n+1] = {};
+   	 for(int i=1;i<=n;i++) cin>>a[i];
+ 
+   	 int i = a[1];
+   	 int j = a[2];
+   	 int k = a[n];
+   	 bool ans = false;
+   	 if(i+j<=k) ans = true;
+   	 else if(k-i>=j) ans = true;
+ 
+   	 if(ans) 
+   	 {
+   	 	cout<<1<<" "<<2<<" "<<n<<"\n";
+   	 }
+   	 else
+   	 {
+   	 	 cout<<-1<<"\n";
+   	 }
+   }
