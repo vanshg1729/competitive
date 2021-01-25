ee9d6e52ab676b5bebada796ae0c62dee37424be
fine-man <44305089+fine-man@users.noreply.github.com>
2021-01-25 13:38:27 +0530

    Update add.cpp

--- CF/Addition and Minimum (segtree part -2)/add.cpp
+++ CF/Addition and Minimum (segtree part -2)/add.cpp
@@ -2,59 +2,105 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define fr first
-#define sc second
 
-const int maxn = pow(10,5) + 10;
-ll INF = 1e15;
+const int maxn = 1e5 + 10;
+const ll mod = 1e9 + 7;
 int n,q;
 
 struct segtree{
 
-	vector<pair<ll,ll>> seg = {};
+	vector<ll> val = {},oper = {};
 
-	void init(int size){
-		size += 10;
-		pair<ll,ll> p1 = {0,0};
-		seg = vector<pair<ll,ll>>(4*size,p1);
+	const ll no_oper = 1;
+	const ll ini_val = 1;
+	const ll no_val = 0;
+
+	ll comb_op(ll a, ll b){
+		return (a + b) % mod;
 	}
 
-	void add(int k, int l, int r, int st, int en, int val){
+	ll val_op(ll a, ll b){
+		return a*b % mod;
+	}
 
-		if(st > en) return;
-		if(st == l and en == r){
-			ll v = (ll) val;
-			seg[k].fr += v;
-			seg[k].sc += v;
+	void apply_op(ll & a, ll b){
+		a = val_op(a, b);
+	}
+
+	void build(int k, int l, int r){
+
+		if(l == r){
+			val[k] = ini_val;
 			return;
 		}
 
-		int m = (l+r)/2;
-		add(2*k, l, m, st, min(en,m), val);
-		add(2*k+1, m+1, r, max(st,m+1), en, val);
-		seg[k].fr = min(seg[2*k].fr, seg[2*k+1].fr) + seg[k].sc;
+		int m = (l + r)/2;
+		build(2*k, l, m);
+		build(2*k+1, m+1, r);
+
+		val[k] = comb_op(val[2*k], val[2*k+1]);
 	}
 
-	void add(int st, int en, int val){
-		add(1, 0, n-1, st, en, val);
+	void init(int size){
+		size += 10;
+		val.assign(4*size, 0);
+		oper.assign(4*size, no_oper);
+		build(1, 0, n-1);
 	}
 
-	ll get_min(int k, int l, int r, int st, int en){
+	void propogate(int k, int l, int r){
 
-		if(st > en) return INF;
+		if( l == r ) return;
+
+		apply_op(val[2*k], oper[k]);
+		apply_op(val[2*k +1], oper[k]);
+		apply_op(oper[2*k], oper[k]);
+		apply_op(oper[2*k +1], oper[k]);
+
+		oper[k] = no_oper;
+	}
+
+	void modify(int k, int l, int r, int st, int en, int va){
+
+		if(st > en) return;
+
+		propogate(k, l, r);
 		if(l == st and r == en){
-			return seg[k].fr;
+			apply_op(val[k], (ll) va);
+			apply_op(oper[k], (ll) va);
+			return;
 		}
 
-		int m = (l+r)/2;
+		int m = (l + r)/2;
+		modify(2*k, l, m, st, min(en, m), va);
+		modify(2*k + 1, m+1, r, max(st, m+1), en, va);
 
-		ll right = get_min(2*k, l, m, st, min(en,m));
-		ll left = get_min(2*k+1, m+1, r, max(st,m+1),en);
-		return seg[k].sc + min(right, left);
+		val[k] = comb_op(val[2*k], val[2*k + 1]);
 	}
 
-	ll get_min(int st, int en){
-		return get_min(1, 0, n-1, st, en);
+	void modify(int st, int en, int va){
+		modify(1, 0, n-1, st, en, va);
+	}
+
+	ll get(int k, int l, int r, int st, int en){
+
+		if(st > en) return no_val;
+
+		propogate(k , l, r);
+		if(l == st and r == en){
+			return val[k];
+		}
+
+		int m = (l + r)/2;
+
+		ll right = get(2*k, l, m, st, min(en,m));
+		ll left = get(2*k + 1, m+1, r, max(st, m+1), en);
+
+		return comb_op(right, left);
+	}
+
+	ll get(int st, int en){
+		return get(1, 0, n-1, st, en);
 	}
 };
 
@@ -79,14 +125,13 @@ int main(){
 
 			int l,r,v;
 			cin>>l>>r>>v;
-			st.add(l, r-1, v);
+			st.modify(l, r-1, v);
 		}
 		else{
 
 			int l,r;
 			cin>>l>>r;
-			cout<<st.get_min(l, r-1)<<"\n";
+			cout<<st.get(l, r-1)<<"\n";
 		}
 	}
-
 }
