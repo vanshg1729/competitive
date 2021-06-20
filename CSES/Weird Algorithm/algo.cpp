b1eee26af0cfc3028e65f88e144619ec6b6007d6
fine-man <44305089+fine-man@users.noreply.github.com>
2021-06-20 17:19:16 +0530

    Create algo.cpp

---
+++ CSES/Weird Algorithm/algo.cpp
@@ -0,0 +1,24 @@
+// https://cses.fi/problemset/task/1068
+#include <bits/stdc++.h>
+using namespace std;
+#define sc second
+#define fr first
+#define ll long long
+const int maxn = 2e5 + 10;
+ 
+int main() {
+ 
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout); 
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n; cin >> n;
+	cout << n << " ";
+	while (n > 1) {
+		if (n % 2) n = 3 * n + 1;
+		else n /= 2;
+		cout << n << " ";
+	}
+
+}
